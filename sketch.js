0e7422f220bf0d2551275f692b185e45d470cc56
Alunisme <413730267@o365.tku.edu.tw>
2025-05-12 11:12:34 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,21 +32,21 @@ function drawFaceMesh() {
     const keypoints = predictions[0].scaledMesh;
 
     // 嘴唇（原本的 indices）
-    drawMeshLine(keypoints, [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291]);
+    drawMeshLine(keypoints, [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291], true, color(255,0,0));
 
-    // 左眼外部
-    drawMeshLine(keypoints, [243,190,56,28,27,29,30,247,130,25,110,24,23,22,26,112], true);
-    // 左眼內部
-    drawMeshLine(keypoints, [133,173,157,158,159,160,161,246,33,7,163,144,145,153,154,155], true);
-    // 右眼外部
-    drawMeshLine(keypoints, [359,467,260,259,257,258,286,444,463,341,256,252,253,254,339,255], true);
-    // 右眼內部
-    drawMeshLine(keypoints, [263,466,388,387,386,385,384,398,362,382,381,380,374,373,390,249], true);
+    // 左眼外部（綠色）
+    drawMeshLine(keypoints, [243,190,56,28,27,29,30,247,130,25,110,24,23,22,26,112,243], true, color(0,255,0));
+    // 左眼內部（綠色）
+    drawMeshLine(keypoints, [133,173,157,158,159,160,161,246,33,7,163,144,145,153,154,155,133], true, color(0,255,0));
+    // 右眼外部（藍色）
+    drawMeshLine(keypoints, [359,467,260,259,257,258,286,444,463,341,256,252,253,254,339,255,359], true, color(0,0,255));
+    // 右眼內部（藍色）
+    drawMeshLine(keypoints, [263,466,388,387,386,385,384,398,362,382,381,380,374,373,390,249,263], true, color(0,0,255));
   }
 }
 
-function drawMeshLine(keypoints, arr, closeShape = true) {
-  stroke(255, 0, 0);
+function drawMeshLine(keypoints, arr, closeShape = true, col = color(255,0,0)) {
+  stroke(col);
   strokeWeight(5);
   noFill();
   beginShape();
